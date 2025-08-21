aba015a482ddfce321aec475a6209dca34482707
itavoro <morovati.tirdad@gmail.com>
2025-08-20 20:13:10 -0400

    create title and introduction cards and create animated gradient background

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-
+import Title from './title.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Title />
   </StrictMode>,
 )
