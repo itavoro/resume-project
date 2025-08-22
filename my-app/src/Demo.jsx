e7ed5a6dc02eb2b1ba4c135aad715d51857606ce
itavoro <morovati.tirdad@gmail.com>
2025-08-22 15:06:29 -0400

    create and animated demo button

--- my-app/src/Demo.jsx
+++ my-app/src/Demo.jsx
@@ -1,19 +1,35 @@
 import { useState } from 'react';
 export default function Demo () {
     function Button (props) {
-        return (
-            <button onClick={props.onClick} id="demoButton" style={{fontSize: props.fontSize, padding: props.padding, height:props.height}} className={`items-center flex w-auto rounded-[50px] border-3 border-green-500`}>
-                <p id="demoText" className="text-center text-green-500 justify-center items">
-                    {props.text}
-                </p>
-            </button>
-        )
+        const [isVisible, setIsVisible] = useState(props.isVisible);
+        const [isAnimating, setIsAnimating] = useState(props.isAnimating);
+     
+        const handleClick = () => {
+            setIsAnimating(true);
+            if (props.onClick) {
+                props.onClick();
+            }
+            setTimeout(() => setIsVisible(false), 1000)
+        };
+        
+        if (isVisible) {
+                return (
+                <button style={{
+                    opacity: isAnimating ? 0 : 1,
+                    transform: isAnimating ? 'scale(0.8)' : 'scale(1)',
+                    transition: 'opacity 0.3s ease-out, transform 0.3s ease-out',
+                    fontSize: props.fontSize,
+                    padding: props.padding,
+                    height:props.height
+                }} onClick={handleClick} id="demoButton" className={`items-center flex w-auto rounded-[50px] border-3 border-green-500`}>
+                    <p id="demoText" className="text-center text-green-500 justify-center items">
+                        {props.text}
+                    </p>
+                </button>
+            )
+        }
     }
 
-    function demoOnClick () {
-        useState(false);
-    }
-    
     return (        
         <div className="bg-black z-50 h-120 w-170 mt-43 ml-30 flex justify-center items-center">
             <Button 
@@ -21,7 +37,9 @@ export default function Demo () {
                  fontSize='40px'
                  padding='30px'
                  height='10px'
-                 onClick={demoOnClick}
+                 isVisible={true}
+                 isAnimating={false}
+                 onClick={() => {console.log('x')}}
             />
         </div>
     )
